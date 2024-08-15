7fc3a5fa4e92070f8df9c8b3558ce4dbdbe58592
ezechiel <yaoezchiel@gmail.com>
2024-08-15 09:25:33 +0000

    j'ai stylisé les différents formulaire

--- dj_mysql/dj_mysql/urls.py
+++ dj_mysql/dj_mysql/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from auth_app import views
 
+# config for css
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.inscription, name="inscription"),
@@ -11,4 +16,9 @@ urlpatterns = [
     path('deconnexion/', views.deconnexion, name="deconnexion"),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
 
